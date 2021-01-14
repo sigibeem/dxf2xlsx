a96bdf3051d1c75f97904acb459b06b8fd544b8c
sigibeem <isbn9778.4.7981@gmail.com>
2021-01-14 21:22:21 +0900

    単一の対象のみを多重ループ化に成功

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+list1 = [["ji", ], ["ji", 'fei', 'fe', 'fe'], ['ji', 'ewi'], ["ji", "few", "jifew", "jfiwe"]]
+list12 = []
+
+
+for i in range(len(list1)):
+    for j in range(len(list1[i])):
+        if list1[i][j] == "ji":
+            sub = []
+            sub.append(list1[i][j])
+        else:
+            continue
+    list12.append(sub)
+print(list12)
