5300889773b44b3d00a576ee6da59162c265a37a
sigibeem <isbn9778.4.7981@gmail.com>
2021-02-04 19:46:38 +0900

    nanndemonai

---
+++ tet.py
@@ -0,0 +1,19 @@
+def Cltst(a, b):
+    c = a + b
+    d = a - b
+    e = a*b
+    f = a/b
+    print(c, d, e, f)
+    return
+
+
+a1 = int(input("１つ目の数値を入力してください："))
+b1 = int(input("２つ目の数値を入力してください"))
+Cltst(a1, b1) 
+
+class Ji():
+    def __init__(self):
+        print("hello")
+        return
+
+Ji()
